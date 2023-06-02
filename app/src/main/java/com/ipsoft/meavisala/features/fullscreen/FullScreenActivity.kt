cb726327ec492dca2c936f1ad0a76640edaaba0c
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-06-02 17:09:44 -0300

    fixed app not starting location service when reopened

--- app/src/main/java/com/ipsoft/meavisala/features/fullscreen/FullScreenActivity.kt
+++ app/src/main/java/com/ipsoft/meavisala/features/fullscreen/FullScreenActivity.kt
@@ -1,27 +1,58 @@
 package com.ipsoft.meavisala.features.fullscreen
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.ipsoft.meavisala.R
+import com.ipsoft.meavisala.core.ui.theme.MeAvisaLaTheme
 import com.ipsoft.meavisala.features.soundalarm.ALARM_DESCRIPTION
 
 class FullScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_full_screen)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        intent.getStringExtra(ALARM_DESCRIPTION)?.let {
-            findViewById<TextView>(R.id.alarm_description)?.apply {
-                text = it
+        setContent {
+            MeAvisaLaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FullScreenContent()
+                }
             }
         }
-        findViewById<Button>(R.id.ok_button)?.setOnClickListener {
-            finish()
+    }
+
+    @Composable
+    fun FullScreenContent() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.background_location_reached_notification_title),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            intent.getStringExtra(ALARM_DESCRIPTION)?.let {
+                Text(text = it, style = MaterialTheme.typography.bodyMedium)
+            }
+            Button(onClick = { finish() }) {
+                Text(
+                    text = getString(R.string.ok),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/ipsoft/meavisala/features/lockscreen/LockScreenActivity.kt
+++ app/src/main/java/com/ipsoft/meavisala/features/lockscreen/LockScreenActivity.kt
@@ -1,10 +1,21 @@
 package com.ipsoft.meavisala.features.lockscreen
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.ipsoft.meavisala.R
+import com.ipsoft.meavisala.core.ui.theme.MeAvisaLaTheme
 import com.ipsoft.meavisala.core.utils.extensions.turnScreenOffAndKeyguardOn
 import com.ipsoft.meavisala.core.utils.extensions.turnScreenOnAndKeyguardOff
 import com.ipsoft.meavisala.features.soundalarm.ALARM_DESCRIPTION
@@ -12,19 +23,39 @@ import com.ipsoft.meavisala.features.soundalarm.ALARM_DESCRIPTION
 class LockScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lock_screen)
+        setContent {
+            MeAvisaLaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FullScreenContent()
+                }
+            }
+        }
         turnScreenOnAndKeyguardOff()
     }
 
-    override fun onResume() {
-        super.onResume()
-        intent.getStringExtra(ALARM_DESCRIPTION)?.let {
-            findViewById<TextView>(R.id.alarm_description)?.apply {
-                text = it
+    @Composable
+    fun FullScreenContent() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.background_location_reached_notification_title),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            intent.getStringExtra(ALARM_DESCRIPTION)?.let {
+                Text(text = it, style = MaterialTheme.typography.bodyMedium)
             }
-        }
-        findViewById<Button>(R.id.ok_button)?.setOnClickListener {
-            finish()
+            Button(onClick = { finish() }) {
+                Text(
+                    text = getString(R.string.ok),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
         }
     }
 
